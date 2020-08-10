8f44b41884be6ae3937778a52d26d195159848a4
likaci <likaci@qq.com>
2020-08-10 18:19:53 +0800

    add notice

--- app/src/main/java/com/xiazhiri/stability/MainActivity.java
+++ app/src/main/java/com/xiazhiri/stability/MainActivity.java
@@ -1,10 +1,11 @@
 package com.xiazhiri.stability;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.Settings;
-import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,10 +15,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "1. 打开无障碍中的 \"稳定性测试\"\n" +
-                "2. 音量+ 开始测试 \n" +
-                "3. 音量- 结束测试", Toast.LENGTH_LONG).show();
-        Intent intent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
-        startActivity(intent);
+
+        new AlertDialog.Builder(this)
+                .setTitle("使用须知")
+                .setMessage(
+                        "此应用仅供测试手机稳定性使用, 严禁用作其他用途, 后果自行承担\n" +
+                                "应用不请求网络/磁盘权限, 请放心使用\n\n" +
+                                "使用方法:\n" +
+                                "1. 打开无障碍中的 \"稳定性测试\"\n" +
+                                "2. 音量+ 开始测试 \n" +
+                                "3. 音量- 结束测试")
+                .setPositiveButton("同意", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        Intent intent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+                        startActivity(intent);
+                    }
+                })
+                .setNegativeButton("退出", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        System.exit(0);
+                    }
+                })
+                .show();
     }
 }
